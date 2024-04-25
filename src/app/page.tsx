a153878f18cb955117a3119c350f4699e4cb9d4f
Dearborn42 <amurf26@outlook.com>
2024-04-25 13:28:22 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <form onSubmit={()=>{
-        'use server'
         handleSubmit
         }}>
         <input 
